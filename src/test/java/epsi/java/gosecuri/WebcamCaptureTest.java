f1f7890662557b41316807678842be6b4e830b39
Fayçal Ziba <Fayal Ziba@192.168.0.12>
2020-07-21 13:30:54 +0200

    ProjetMsprJAVA

---
+++ src/test/java/epsi/java/gosecuri/WebcamCaptureTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epsi.java.gosecuri;
+
+import org.junit.*;
+
+import static junit.framework.TestCase.*;
+
+/**
+ *
+ * @author mmf
+ */
+public class WebcamCaptureTest {
+    
+    public WebcamCaptureTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testDisplay() {
+        assertTrue(true == true);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
